7dfee2501cf565cf3ced3bd7d37c79b38a077e53
Varun Devgan <varun.devgan@outlook.com>
2025-07-09 23:04:40 +0530

    done

--- designOne/script.js
+++ designOne/script.js
@@ -1,9 +1,22 @@
 const toggleBtn = document.getElementById('btn')
 const sidebar = document.getElementById('sidebar')
 const overlay = document.getElementById('overlay')
+const navbar = document.getElementById('navbar')
+const img = document.querySelector('img')
+const slider = document.getElementById('slider')
+let pageY = 0
 
-console.log(window.screen)
-console.log(window.innerWidth)
+
+window.addEventListener('scroll',()=>{
+    if(window.pageYOffset > pageY && window.pageYOffset > 80){
+        navbar.classList.add('-translate-y-full')
+    }
+    else {
+        navbar.classList.remove('-translate-y-full')
+    }
+    pageY = window.pageYOffset
+
+})
 
 window.addEventListener('resize',()=>{
     if(window.innerWidth >= 768){
@@ -14,6 +27,7 @@ window.addEventListener('resize',()=>{
 
 toggleBtn.addEventListener('click',()=>{
     sidebar.classList.remove('-translate-x-full')
+    sidebar.classList.add('bg-blue-400')
     overlay.classList.remove('hidden')
 })
 
@@ -21,3 +35,9 @@ overlay.addEventListener('click',()=>{
     sidebar.classList.add('-translate-x-full')
     overlay.classList.add('hidden')
 })
+
+
+
+
+
+
